87418e51e8e0b473274d42bc80780708d52b02f2
sakib8900 <md.sakib388900@gmail.com>
2025-01-09 11:32:04 +0600

    deploy

--- src/utilitis/Loading.jsx
+++ src/utilitis/Loading.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Loading = () => {
-    // console.log('Loading component rendered');
     return (
-        <div className='flex min-h-screen justify-center items-center'>
-            <span className="loading loading-bars loading-lg"></span>
+        <div className='flex min-h-screen justify-center items-center bg-gray-50'>
+            <div className='relative w-16 h-16'>
+                <div className='absolute inset-0 border-4 border-t-red-500 border-b-red-500 border-l-transparent border-r-transparent rounded-full animate-spin'></div>
+                <div className='absolute inset-2 border-4 border-t-red-300 border-b-red-300 border-l-transparent border-r-transparent rounded-full animate-spin-slower'></div>
+            </div>
         </div>
     );
 };
